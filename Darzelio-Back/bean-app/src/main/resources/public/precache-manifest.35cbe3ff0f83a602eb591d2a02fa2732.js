a4e58611e4d32cbfc1659665fb59a8b1b2d4d7e9
Abcrusne <rusne.pet@gmail.com>
2021-01-21 12:52:39 +0200

    ir back ir front vienoj repo

---
+++ Darzelio-Back/bean-app/src/main/resources/public/precache-manifest.35cbe3ff0f83a602eb591d2a02fa2732.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649d9f0c8e159a15a2f8d163bd5e7670",
+    "url": "/bean-app/index.html"
+  },
+  {
+    "revision": "d5482bc17377c92bfcd5",
+    "url": "/bean-app/static/css/main.fc6d0e1e.chunk.css"
+  },
+  {
+    "revision": "2e59ade559753bf5f492",
+    "url": "/bean-app/static/js/2.37a4363c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/bean-app/static/js/2.37a4363c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec19e60b5419e3d9453",
+    "url": "/bean-app/static/js/3.a8c87af0.chunk.js"
+  },
+  {
+    "revision": "d5482bc17377c92bfcd5",
+    "url": "/bean-app/static/js/main.f7b9ff9a.chunk.js"
+  },
+  {
+    "revision": "61f4a8915a7110c28684",
+    "url": "/bean-app/static/js/runtime-main.08f1cfbc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/bean-app/static/media/logo.06e73328.svg"
+  }
+]);
